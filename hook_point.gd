53474c0607953c3efd3e4c8eb290f5abe0a9f8f7
Person552 <qaustinbarnes@gmail.com>
2026-06-22 22:54:39 -0700

    hookpoint colors
    hookpoint colors now display properly in editor

--- hook_point.gd
+++ hook_point.gd
@@ -1,13 +1,12 @@
+@tool
 extends Node2D
 
-@export_enum("swing", "pull", "loop") var type = "swing"
+@export_enum("swing", "pull", "loop") var type = "swing" :
+	set(new_type) :
+		type = new_type
+		$AnimatedSprite2D.animation = type
 @export var id : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.animation = type
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
